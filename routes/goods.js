358e5fe009f6eeddb8bc7b4e0a0625486dc7e452
c4fiber <qudcjf153@gmail.com>
2023-11-01 17:45:18 +0900

    feat: 특정 물품의 정보를 얻는 api 구현

--- routes/goods.js
+++ routes/goods.js
@@ -12,3 +12,50 @@ router.get("/about", (req, res) => {
 });
 
 module.exports = router;
+
+// /routes/goods.js
+const goods = [
+  {
+    goodsId: 4,
+    name: "상품 4",
+    thumbnailUrl:
+      "https://cdn.pixabay.com/photo/2016/09/07/02/11/frogs-1650657_1280.jpg",
+    category: "drink",
+    price: 0.1,
+  },
+  {
+    goodsId: 3,
+    name: "상품 3",
+    thumbnailUrl:
+      "https://cdn.pixabay.com/photo/2016/09/07/02/12/frogs-1650658_1280.jpg",
+    category: "drink",
+    price: 2.2,
+  },
+  {
+    goodsId: 2,
+    name: "상품 2",
+    thumbnailUrl:
+      "https://cdn.pixabay.com/photo/2014/08/26/19/19/wine-428316_1280.jpg",
+    category: "drink",
+    price: 0.11,
+  },
+  {
+    goodsId: 1,
+    name: "상품 1",
+    thumbnailUrl:
+      "https://cdn.pixabay.com/photo/2016/09/07/19/54/wines-1652455_1280.jpg",
+    category: "drink",
+    price: 6.2,
+  },
+];
+
+// localhost:3000/api/goods GET
+router.get("/goods", (req, res) => {
+  res.json({ goods: goods});
+});
+
+// localhost:3000/api/goods/? GET
+router.get("/goods/:goodsId", (req, res) => {
+  // return that goodsId == req.params.goodsId
+  res.json({ goods: goods.find((goods) => goods.goodsId == req.params.goodsId) });
+});
